79b7ae39ed425821384ae672d7ec17dd353002ca
春拓 藤巻 <syunta_0420@icloud.com>
2023-10-12 10:31:54 +0900

    Enemy生成

--- DirectXGame/Enemy.h
+++ DirectXGame/Enemy.h
@@ -1,2 +1,33 @@
 ﻿#pragma once
+#include "WorldTransform.h"
+#include "Model.h"
+#include "Input.h"
 
+
+class Enemy {
+public:
+	////デストラクタ
+	//~Enemy();
+
+	void Initialize(Model* model, uint32_t textureHandle);
+
+	void Update();
+
+	void Draw(ViewProjection& viewProjection);
+
+	// デスフラグ
+	bool isDead_ = false;
+
+private:
+	void Approach();//接近
+	// ワールド変換データ
+	WorldTransform worldTransform_;
+	// モデル
+	Model* model_ = nullptr;
+	// テクスチャハンドル
+	uint32_t textureHandle_ = 0u;
+	// キーボード入力
+	Input* input_ = nullptr;
+
+
+};
