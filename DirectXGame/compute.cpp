0fa5a046c262038e1fd286ed7c37132c28dafe2a
春拓 藤巻 <syunta_0420@icloud.com>
2023-10-12 11:01:02 +0900

    敵の平行移動

--- DirectXGame/compute.cpp
+++ DirectXGame/compute.cpp
@@ -37,78 +37,39 @@ Vector3 Normalize(const Vector3& v) {
 
 // X軸回転行列
 Matrix4x4 MakeRotateXmatrix(float radian) {
-	Matrix4x4 result;
-	result.m[0][0] = 1.0f;
-	result.m[0][1] = 0.0f;
-	result.m[0][2] = 0.0f;
-	result.m[0][3] = 0.0f;
-
-	result.m[1][0] = 0.0f;
-	result.m[1][1] = std::cos(radian);
-	result.m[1][2] = std::sin(radian);
-	result.m[1][3] = 0.0f;
-
-	result.m[2][0] = 0.0f;
-	result.m[2][1] = -std::sin(radian);
-	result.m[2][2] = std::cos(radian);
-	result.m[2][3] = 0.0f;
-
-	result.m[3][0] = 0.0f;
-	result.m[3][1] = 0.0f;
-	result.m[3][2] = 0.0f;
-	result.m[3][3] = 1.0f;
+	Matrix4x4 result = {0};
+	result.m[0][0] = cosf(radian);
+	result.m[0][2] = -sinf(radian);
+	result.m[1][1] = 1;
+	result.m[2][0] = sinf(radian);
+	result.m[2][2] = cosf(radian);
+	result.m[3][3] = 1;
 	return result;
 }
 
 // Y軸回転行列
 Matrix4x4 MakeRotateYmatrix(float radian) {
-	Matrix4x4 result;
-	result.m[0][0] = std::cos(radian);
-	result.m[0][1] = 0.0f;
-	result.m[0][2] = -std::sin(radian);
-	result.m[0][3] = 0.0f;
-
-	result.m[1][0] = 0.0f;
-	result.m[1][1] = 1.0f;
-	result.m[1][2] = 0.0f;
-	result.m[1][3] = 0.0f;
-
-	result.m[2][0] = std::sin(radian);
-	result.m[2][1] = 0.0f;
-	result.m[2][2] = std::cos(radian);
-	result.m[2][3] = 0.0f;
-
-	result.m[3][0] = 0.0f;
-	result.m[3][1] = 0.0f;
-	result.m[3][2] = 0.0f;
-	result.m[3][3] = 1.0f;
+	Matrix4x4 result = {0};
+	result.m[0][0] = cosf(radian);
+	result.m[0][2] = -sinf(radian);
+	result.m[1][1] = 1;
+	result.m[2][0] = sinf(radian);
+	result.m[2][2] = cosf(radian);
+	result.m[3][3] = 1;
 	return result;
-}
+};
 
 // Z軸回転行列
 Matrix4x4 MakeRotateZmatrix(float radian) {
-	Matrix4x4 result;
-	result.m[0][0] = std::cos(radian);
-	result.m[0][1] = std::sin(radian);
-	result.m[0][2] = 0.0f;
-	result.m[0][3] = 0.0f;
-
-	result.m[1][0] = -std::sin(radian);
-	result.m[1][1] = std::cos(radian);
-	result.m[1][2] = 0.0f;
-	result.m[1][3] = 0.0f;
-
-	result.m[2][0] = 0.0f;
-	result.m[2][1] = 0.0f;
-	result.m[2][2] = 1.0f;
-	result.m[2][3] = 0.0f;
-
-	result.m[3][0] = 0.0f;
-	result.m[3][1] = 0.0f;
-	result.m[3][2] = 0.0f;
-	result.m[3][3] = 1.0f;
+	Matrix4x4 result = {0};
+	result.m[0][0] = cosf(radian);
+	result.m[0][1] = sinf(radian);
+	result.m[1][0] = -sinf(radian);
+	result.m[1][1] = cosf(radian);
+	result.m[2][2] = 1;
+	result.m[3][3] = 1;
 	return result;
-}
+};
 
 // 平行移動
 Matrix4x4 MakeTranslateMatrix(Vector3 translate) {
